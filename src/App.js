cbceb094fffc75c1060f51455301dcad08bc22cb
sathish <2000sathishkumar.d@gmail.com>
2023-03-29 20:59:21 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import react icons
+import { FiChevronRight, FiChevronLeft, FiChevronsRight } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+// import data
+import data from "./data";
 
-const App = () => {
-  return <div className="text-2xl my-5 font-bold underline">App</div>;
-};
+// function
+function App() {
+  const [people, setPeople] = useState(data);
+  // console.log(data);
+  const [index, setIndex] = useState(0);
+  // console.log(people);
+
+  // using the use Effect
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+  // set interval function
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="font-bold title">
+        <h2>
+          <span className="text-orange-700">/</span>review
+        </h2>
+      </div>
+      <div className="section-container">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          // more step coming up
+          let position = "nextSlide";
+
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && position === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="image" />
+              <h3 className="name">{name}</h3>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,51 @@
+import { data } from "autoprefixer";
+
+const people = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    name: "Emma",
+    title: "team leader",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio deleniti suscipit molestias, minima eaque voluptatem!",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "Jayden",
+    title: "Hr team",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat commodi eveniet, molestiae quae dignissimos nisi.",
+  },
+  {
+    id: 3,
+    image:
+      "https://www.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg",
+    name: "Amelia",
+    title: "office worker",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas placeat tempora soluta? Architecto, odit ratione.",
+  },
+  {
+    id: 4,
+    image:
+      "https://media.istockphoto.com/id/621603944/photo/beautiful-teenage-girl.jpg?s=612x612&w=0&k=20&c=p9e5SFhooPXyv0QuIvxxpstwVqADywMM09uftuUV0VI=",
+    name: "Rowan Kai",
+    title: "regular guy",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis quaerat, neque ipsum nostrum culpa vel.",
+  },
+  {
+    id: 5,
+    image:
+      "https://img.freepik.com/free-photo/concentrated-young-man-sitting-bench-reading-newspaper-outdoors_171337-14758.jpg",
+    name: "Kayden",
+    title: "office manager",
+    quote:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse quibusdam cupiditate hic. Ut, vel corrupti?",
+  },
+];
+
+export default people;
